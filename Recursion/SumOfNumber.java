842deb819bf1d4ad8adf7da926c2b0ed6f1baecd
Harun Ansari <harunansaru051@gmail.com>
2025-09-10 21:36:20 +0530

    Sum of N Number

---
+++ Recursion/SumOfNumber.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class SumOfNumber {
+    public static void main(String[] args) {
+        int ans = sum(5) ;
+        System.out.println(ans);
+        
+    }
+
+
+    static int sum (int n) {
+        if(n == 1) {
+            return 1 ;
+        }
+
+        return n + sum(n-1) ;
+    }
+}
