fdad6978a110e5fae7d4dfb41f0ee0be0066728f
Harun Ansari <harunansaru051@gmail.com>
2025-09-10 21:24:04 +0530

    Print one to N Using recursion

---
+++ Recursion/OnetoN.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+public class OnetoN {
+    public static void main(String[] args) {
+        print1toN(10);
+        
+    }
+
+
+    static void print1toN(int n ) {
+        if(n== 0)
+        return ; 
+        
+
+        print1toN(n-1);
+
+        System.out.print(n +" ");
+
+        
+    }
+}
