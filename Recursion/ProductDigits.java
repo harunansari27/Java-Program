97c4d664546134efb5c629c1c68c4811029ac1df
Harun Ansari <harunansaru051@gmail.com>
2025-09-11 13:49:29 +0530

    Product of the digits

---
+++ Recursion/ProductDigits.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+public class ProductDigits {
+
+    public static void main(String [] args) {
+
+        int ans = prod(55) ;
+        System.out.println(ans);
+
+    }
+
+
+    static int prod (int n ) {
+        if(n%10 ==n)
+        return n ; 
+return (n%10) * prod(n/10) ;
+    }
+}
