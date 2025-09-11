dd2015ca6f1bf48d47126efd06de153f60c43f6c
Harun Ansari <harunansaru051@gmail.com>
2025-09-11 13:44:00 +0530

    Sum of the Digits

---
+++ Recursion/DigitSum.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+public class DigitSum {
+
+    public static void main(String[] args) {
+
+        int num = 12345 ; 
+        System.out.println("Sum of the Digits : " +sum(num)); 
+        
+    }
+
+
+    static int sum (int n ) {
+
+        if(n == 0 ){
+            return 0;
+        } 
+
+        return n%10  + sum(n/10)  ;
+    }
+    
+}
