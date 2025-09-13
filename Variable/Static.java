fc75795f44196276ccbc7979ffff0aadc8a316b0
Harun Ansari <harunansaru051@gmail.com>
2025-09-13 10:16:47 +0530

    Also call by class.var to get out

---
+++ Variable/Static.java
@@ -0,0 +1,15 @@
+package Variable;
+
+public class Static {
+
+    static int a = 10 ; // Static Variable
+
+    static int b = 20 ; // Instance Variable
+
+    public static void main(String[] args) {
+        System.out.println("Value of a: " + a); // Output: Value of a: 10
+
+        System.out.println("Value of b: " + Static.b); // Output: Value of
+
+    }
+}
