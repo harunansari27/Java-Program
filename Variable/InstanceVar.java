79f11a28b2f666cf9c9312233cc8d14350fc3e71
Harun Ansari <harunansaru051@gmail.com>
2025-09-13 10:11:13 +0530

    By default value of var for int is 0

--- Variable/InstanceVar.java
+++ Variable/InstanceVar.java
@@ -2,7 +2,10 @@ package Variable;
 
 public class InstanceVar {
 
-    int a  = 20 ; // Instance Variable
+   // int a  = 20 ; // Instance Variable
+
+   int a ; 
+    // Instance Variable with default value 0 for int data type
     public static void main(String[] args) {
 
         InstanceVar obj = new InstanceVar();
@@ -18,4 +21,11 @@ public class InstanceVar {
 // In the main method, we create an object of the InstanceVar class and access the instance variable 'a' using that object.
 // The output will be "Value of a: 20" as expected.
 // Instance variables are associated with an instance of a class and can be accessed using the object of that class.
+//=======================================================================
+
+// 🔹 Note
+// If we do not initialize the instance variable 'a', it will have a default value of 0 for int data type.
+// For example:
+// int a ;
+// The output will be "Value of a: 0" in that case.
 //=======================================================================
