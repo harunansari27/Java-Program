b261005b9b195cbb4b408c999cb95397b3da18c6
Harun Ansari <harunansaru051@gmail.com>
2025-09-22 19:18:09 +0530

    Intialisation of object or intialised value

--- OOPs/Students.java
+++ OOPs/Students.java
@@ -24,9 +24,18 @@ class Students {
 
     Stud harun = new Stud();
 
+    harun.rno = 1;
+    harun.name = "Harun";   
+    harun.marks = 90.5f;
+
+    System.out.println("---------------------------------------");
     System.out.println(harun);
-    System.out.println(harun.rno);
-    System.out.println(harun.name);
+    System.out.println("1. Roll No Of Student: "+harun.rno);
+    System.out.println("2. Name Of Student: "+harun.name);
+    System.out.println("3. Marks Of Student: "+harun.marks);
+    System.out.println("4. City Of Student: "+harun.city);
+    System.out.println("5. State Of Student: "+harun.state);
+    System.out.println("---------------------------------------");
 
     }
 }
@@ -35,5 +44,7 @@ class Stud {
     int rno;
     String name;
     float marks;
+    String city = "Deoria";
+    String state = "Uttar Pradesh";
 }
 
