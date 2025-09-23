8e9f4f6572c487d847c4f8dc037de238b6b7aff0
Harun Ansari <harunansaru051@gmail.com>
2025-09-23 08:22:57 +0530

    Practice Package

---
+++ OOPs/Class1/Greeting.java
@@ -0,0 +1,9 @@
+package Class1;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("This is Packages Class in Class1 Package");
+        // write greeting , message , wish etc.
+        System.out.println("Hello, Welcome to Java Programming");
+    }
+}
