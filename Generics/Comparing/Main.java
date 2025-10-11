a291723881f472ccad545e4db9cd0e6e0b2780af
Harun Ansari <harunansaru051@gmail.com>
2025-10-11 08:03:46 +0530

    Comparing object data

--- Generics/Comparing/Main.java
+++ Generics/Comparing/Main.java
@@ -1,18 +1,42 @@
 package Generics.Comparing;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class Main {
 
   public static void main(String[] args) {
     
     Students harun = new Students(2, 88.9f);
     Students kyum = new Students(5, 92.5f);
+    Students arif = new Students(3, 88.9f);
+    Students zia = new Students(1, 75.4f);
+    Students sami = new Students(4, 92.5f); 
 
-    if(harun < kyum){
-      System.out.println("Harun has less percentage than Kyum");
-    } 
-    else {
-      System.out.println("Kyum has less percentage than Harun");
-    }
+    Students[] student = {harun, kyum, arif, zia, sami};
+
+    System.out.println(Arrays.toString(student));
+    Arrays.sort(student , new Comparator<Students>() {
+      @Override
+      public int compare(Students o1, Students o2) {
+        return (int)(o1.percentage - o2.percentage);
+      }
+      
+    });
+    System.out.println(Arrays.toString(student)); 
+
+    // if(harun.compareTo(kyum) < 0){
+    //   System.out.println(harun.compareTo(kyum));
+    //   System.out.println("Harun has less percentage than Kyum");
+    // } 
+    // else if(harun.compareTo(kyum) > 0){
+    //   System.out.println(harun.compareTo(kyum));
+    //   System.out.println("Harun has more percentage than Kyum");
+    // }
+    // else{
+    //   System.out.println(harun.compareTo(kyum));
+    //   System.out.println("Harun and Kyum have same percentage");
+    // }
    
   }
    
--- Generics/Comparing/Students.java
+++ Generics/Comparing/Students.java
@@ -1,13 +1,33 @@
 package Generics.Comparing;
 
-public class Students {
+public class Students implements Comparable<Students> {
 
-  public int rollNo;
-  public float percentage ;
+    public int rollNo;
+    public float percentage;
 
-  public Students(int rollNo, float percentage) {
-   this.rollNo = rollNo;
-   this.percentage = percentage;
-  }
-  
+    public Students(int rollNo, float percentage) {
+        this.rollNo = rollNo;
+        this.percentage = percentage;
+    }
+
+    @Override
+    public String toString() {
+     // System.out.println("toString method called");
+        return rollNo + " " + percentage;
+    }
+
+    @Override
+    public int compareTo(Students o) {
+        // Better: use Float.compare to avoid precision loss
+       int diff = (int)(this.percentage - o.percentage);
+       return diff;
+    }
+
+    // @Override
+    // public String toString() {
+    //     return "Students{" +
+    //             "rollNo=" + rollNo +
+    //             ", percentage=" + percentage +
+    //             '}';
+    // }
 }
