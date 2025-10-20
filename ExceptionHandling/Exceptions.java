9229e62625a0f2314cf78982863b83c7d745d2d7
Harun Ansari <harunansaru051@gmail.com>
2025-10-20 23:28:59 +0530

    Own Exception handling

--- ExceptionHandling/Exceptions.java
+++ ExceptionHandling/Exceptions.java
@@ -7,11 +7,17 @@ public class Exceptions {
     int a = 10;
     int b = 0;
 
-    divide(a, b) ;
+    
     try {
-      int c = a / b;
-      System.out.println("Result: " + c);
-    } catch (ArithmeticException e) {
+     // divide(a, b) ;
+      String name = "Harun" ;
+      if (name.equals("Harun")) {
+        throw new MyException("Name is Harun");
+      }
+    } catch (MyException e) {
+      System.out.println(e.getMessage() );
+    } 
+    catch (ArithmeticException e) {
       System.out.println("Error: Division by zero is not allowed.");
     } catch (Exception e) {
       System.out.println(e.getMessage());
---
+++ ExceptionHandling/MyException.java
@@ -0,0 +1,11 @@
+package ExceptionHandling;
+
+public class MyException  extends Exception {
+
+  public MyException(String message) {
+    super(message);
+  }
+  public static void main(String[] args) {
+    
+  }
+}
