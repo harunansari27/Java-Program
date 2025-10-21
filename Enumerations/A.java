59b53311f6d74f1297ea90784f5d5036922e6155
Harun Ansari <harunansaru051@gmail.com>
2025-10-21 16:22:50 +0530

    Interface and valueOf Methods use

---
+++ Enumerations/A.java
@@ -0,0 +1,7 @@
+
+ package Enumerations;
+
+public interface A {
+void hello ();
+  
+}
--- Enumerations/Basics.java
+++ Enumerations/Basics.java
@@ -21,7 +21,7 @@ public class Basics {
       RED, GREEN, BLUE ; 
    }
 
-   enum Months {
+   enum Months implements A {
       JANUARY,
       FEBRUARY,
       MARCH,
@@ -42,16 +42,20 @@ public class Basics {
          // enum constructors are private by default
          // cannot be public or protected
          // internally : public static final Months JANUARY = new Months();
-
-        
       }
 
+      @Override
+      public void hello() {
+         System.out.println("Hello from Months enum");  
 
+      }
   
    }
 
    public static void main(String[] args) {
       Months month = Months.MARCH;
+      month.hello();
+      System.out.println(month.valueOf("MARCH")); // returns the enum constant of the specified string value  
     
       // for (Months  mon : Months.values()) {
       //    System.out.println(mon);
