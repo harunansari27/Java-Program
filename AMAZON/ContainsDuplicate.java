47f2d546616f317e0c04cd6341b6dd5eed5a7f0c
Harun Ansari <harunansaru051@gmail.com>
2025-12-30 01:28:18 +0530

    Check Contains Duplicate Or Not

---
+++ AMAZON/ContainsDuplicate.java
@@ -0,0 +1,27 @@
+package AMAZON;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+  
+  public static boolean containsDuplicate(int[] nums) {
+    HashSet<Integer> set = new HashSet<>();
+
+    for(int num : nums ){
+      if(set.contains(num)) {
+        return true;
+      } 
+      else {
+        set.add(num);
+      }
+      
+     }
+    return false;
+  }
+
+
+  public static void main(String[] args) {
+    int[] nums = {1,2,3};
+    System.out.println(containsDuplicate(nums));
+  }
+}
