1faffc21f4c95a9f3e0f10edbf4f57c13f12068a
Harun Ansari <harunansaru051@gmail.com>
2026-01-04 23:00:04 +0530

    Pre- Order

---
+++ Tree/BinaryTree.java
@@ -0,0 +1,36 @@
+package Tree;
+
+public class BinaryTree {
+  
+  static class Node {
+    int data ;
+    Node left, right;
+
+    Node(int data) {
+      this.data = data;
+      left = right = null;
+    }
+  }
+
+  static class BinaryTreeImpl {
+    static int idx = -1;
+    public static Node buildTree(int nodes[]) {
+      idx++;
+      if (nodes[idx] == -1) {
+        return null;
+      }
+      Node newNode = new Node(nodes[idx]);
+      newNode.left = buildTree(nodes);
+      newNode.right = buildTree(nodes);
+      return newNode;
+    }
+
+}
+
+  public static void main(String[] args) {
+    int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+    BinaryTreeImpl tree = new BinaryTreeImpl();
+    Node root = tree.buildTree(nodes);
+    System.out.println(root.data);
+  }
+}
